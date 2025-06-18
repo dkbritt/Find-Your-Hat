35833ec7b323ce832286fd9a003c26bbf5095b83
Dbraxton6 <daishakb@gmail.com>
2025-06-18 12:41:56 -0700

    Setup user input

--- main.js
+++ main.js
@@ -27,3 +27,33 @@ const myField = new Field([
 
 
 myField.print();
+
+
+function promptMove() {
+    let move = prompt("Which direction would you like to move? ('L'-left, 'R'-right, 'U'-up, 'D'-down): ");
+    move = move.toLowerCase();
+
+    if (move === 'l' || move === 'r' || move === 'u' || move === 'd') {
+        makeMove(move)
+    } else {
+        console.log('Please enter a valid direction.');
+        promptMove();
+    }
+}
+
+function makeMove(move) {
+    if (move === 'l') {
+
+    } else if (move === 'r') {
+
+    } else if (move === 'u') {
+
+    } else if (move === 'd') {
+        
+    }
+}
+
+promptMove();
+
+
+
