7e6f402fdbaecde2931e268d1887e57043db3c35
Dbraxton6 <daishakb@gmail.com>
2025-06-18 18:42:49 -0700

    Generate custom fields and run game

--- main.js
+++ main.js
@@ -7,18 +7,10 @@ const pathCharacter = '*';
 
 
 class Field {
-    constructor(field, playerRow, playerCol) {
+    constructor(field) {
         this.field = field;
-        
-        // find start point of pathCharacter
-        for (let row = 0; row < field.length; row++) {
-            for (let col = 0; col < field[row].length; col++) {
-                if (field[row][col] === pathCharacter) {
-                    this.playerRow = playerRow;
-                    this.playerCol = playerCol;
-                }
-            }
-        }
+        this.playerRow = 0;
+        this.playerCol = 0;
     }
 
     print() {
@@ -67,15 +59,112 @@ class Field {
 
         return 'continue';
     }
+
+    static generateField(width, height, percentage) {
+        // Create 2d field with width/height and fill with field characters '░'
+        const field = [];
+        for (let row = 0; row < height; row++) {
+            field[row] = [];
+            for (let col = 0; col < width; col++) {
+                field[row][col] = fieldCharacter;
+            }
+        }
+
+        // Set pathCharacter '*' to starting position
+        field[0][0] = pathCharacter;
+
+        // Place hat randomly (avoid [0][0])
+        let hatRow = 0;
+        let hatCol = 0;
+
+        while (hatRow === 0 && hatCol === 0) {
+            hatRow = Math.floor(Math.random() * height);
+            hatCol = Math.floor(Math.random() * width);
+        }
+
+        field[hatRow][hatCol] = hat;
+
+
+        // Place holes (avoid [0][0] and hat position)
+        const totalCells = width * height;
+        const numOfHoles = Math.floor(totalCells * percentage);
+        let holesPlaced = 0;
+
+        while (holesPlaced < numOfHoles) {
+            let holeRow = Math.floor(Math.random() * height);
+            let holeCol = Math.floor(Math.random() * width);
+
+            // Avoid hat and [0][0] positions. Dont overwrite another hole
+            if ((holeRow !== 0 || holeCol !== 0) && field[holeRow][holeCol] === fieldCharacter) {
+                field[holeRow][holeCol] = hole;
+                holesPlaced++;
+            }
+        }
+
+        return field;
+        
+    }
 }
 
 
-const myField = new Field([
-  ['*', '░', 'O'],
-  ['░', 'O', '░'],
-  ['░', '^', '░'],
-], 0, 0);
+// const myField = new Field([
+//   ['*', '░', 'O'],
+//   ['░', 'O', '░'],
+//   ['░', '^', '░'],
+// ], 0, 0);
 
+
+function fieldSetup() {
+    let validWidth, validHeight, validPercent = false;
+    let fieldWidth, fieldHeight, fieldPercent;
+
+    console.log("\n\nWelcome to 'Find-Your-Hat'! Let's create a custom field.");
+    console.log("***                                                  ***\n");
+
+
+    while (!validWidth) {
+        fieldWidth = prompt("Enter a field width (between 2-50): ");
+        fieldWidth = Number(fieldWidth);
+
+        // If the fieldWidth entered is Not-a-number, or is a number, but not between 2-10, then ask the user to try again
+        if (isNaN(fieldWidth) || (typeof fieldWidth === 'number' && (fieldWidth < 2 || fieldWidth > 50))) {
+            console.log('Sorry, please enter a valid width.');
+        } else {
+            validWidth = true;
+        }      
+    }
+
+    while (!validHeight) {
+        fieldHeight = prompt("Enter a field height (between 2-50): ");
+        fieldHeight = Number(fieldHeight);
+
+        // If the fieldWidth entered is Not-a-number, or is a number, but not between 2-10, then ask the user to try again
+        if (isNaN(fieldHeight) || (typeof fieldHeight === 'number' && (fieldHeight < 2 || fieldHeight > 50))) {
+            console.log('Sorry, please enter a valid height.');
+        } else {
+            validHeight = true;
+        }  
+    }
+
+    while (!validPercent) {
+        fieldPercent = prompt("What percentage of the board should be holes? (Max is 50): ");
+        fieldPercent = Number(fieldPercent);
+
+        // If the fieldWidth entered is Not-a-number, or is a number, but not between 2-10, then ask the user to try again
+        if (isNaN(fieldPercent) || (typeof fieldPercent === 'number' && (fieldPercent < 0 || fieldPercent > 50))) {
+            console.log('Sorry, please enter a valid percentage (do not inclue the % symbol).');
+        } else {
+            validPercent = true;
+        }   
+    }    
+
+    // let numOfHoles = fieldWidth * fieldHeight
+
+    console.log(`Your field is ${fieldWidth} wide, ${fieldHeight} tall. ${fieldPercent}% of holes`);
+
+    return [fieldWidth, fieldHeight, fieldPercent]
+
+}
 
 function promptMove() {
     let move = prompt("Which direction would you like to move? ('L'-left, 'R'-right, 'U'-up, 'D'-down): ");
@@ -93,13 +182,11 @@ function promptMove() {
     }
 }
 
-function play() {
-    myField.print();
-    promptMove();
-}
-
-// Run the game
-play();
+const [width, height, percent] = fieldSetup(); // get user input
+const generatedField = Field.generateField(width, height, percent/100); // generate the 2D array
+const myField = new Field(generatedField); // create Field instance at starting position
+myField.print();
+promptMove();
 
 
 
